2962fab730c41aba09f420a385b17a674e536a8e
Merlin <Merlin@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2009-08-06 09:47:58 +0000

    Renamed reader.h into card.h which is more correct (2)
    git-svn-id: https://svn.streamboard.tv/oscam/branches/merlin@174 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- include/card.h
+++ include/card.h
@@ -1,9 +1,9 @@
-#ifndef __READER_H__
-#  define __READER_H__
+#ifndef __CARD_H__
+#  define __CARD_H__
 
 extern void cs_ri_brk(int);
 extern void cs_ri_log(char *, ...);
 extern void start_cardreader(void);
 extern void reader_card_info(void);
 
-#endif // __READER_H__
+#endif // __CARD_H__
