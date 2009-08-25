a2b7d20cb4f34aeafb761b773a84ab85cc161d33
Merlin <Merlin@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2009-08-25 09:24:28 +0000

    reader_serial_cmd2reader() is now static
    git-svn-id: https://svn.streamboard.tv/oscam/branches/merlin@230 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- include/reader/serial.h
+++ include/reader/serial.h
@@ -2,7 +2,6 @@
 #  define __READER_SERIAL_H__
 
 int reader_serial_cmd2card(uchar *, ushort, uchar *, ushort, ushort *);
-int reader_serial_cmd2reader(uchar *, ushort, uchar *, ushort, ushort *);
 
 int reader_serial_activate_card(uchar *, ushort *);
 int reader_serial_card_inserted();
--- src/reader/serial.c
+++ src/reader/serial.c
@@ -88,7 +88,7 @@ int reader_serial_cmd2card(uchar *cmd, ushort cmd_size, uchar *result, ushort re
 	return reader_serial_do_api(0, cmd, cmd_size, result, result_max_size, result_size, D_DEVICE);
 }
 
-int reader_serial_cmd2reader(uchar *cmd, ushort cmd_size, uchar *result, ushort result_max_size, ushort *result_size)
+static int reader_serial_cmd2reader(uchar *cmd, ushort cmd_size, uchar *result, ushort result_max_size, ushort *result_size)
 {
 	return reader_serial_do_api(1, cmd, cmd_size, result, result_max_size, result_size, D_DEVICE);
 }
