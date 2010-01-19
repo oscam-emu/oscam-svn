46a125b58da6adc974ae14ede54e96f371b6e0bd
rorothetroll <rorothetroll@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-01-19 15:48:50 +0000

    resync with trunk
    git-svn-id: https://svn.streamboard.tv/oscam/branches/smartreader@1290 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- csctapi/protocol_t0.h
+++ csctapi/protocol_t0.h
@@ -41,54 +41,17 @@
 #define PROTOCOL_T0_IFD_ERROR		3	/* IFD comunication error */
 #define PROTOCOL_T0_ERROR		4	/* T=0 Protocol Error */
 
-/* Return codes */
-#define PROTOCOL_T14_OK			0	/* Command OK */
-#define PROTOCOL_T14_NULL_ERROR		1	/* Maximum NULL's reachec */
-#define PROTOCOL_T14_ICC_ERROR		2	/* ICC comunication error */
-#define PROTOCOL_T14_IFD_ERROR		3	/* IFD comunication error */
-#define PROTOCOL_T14_ERROR		4	/* T=0 Protocol Error */
-
 /*
  * Exported datatypes definition
  */
-
-/* T=0 Protocol handler */
-typedef struct
-{
-  unsigned long wwt;		/* Work waiting time (ms) */
-}
-Protocol_T0;
-
-/* T=14 Protocol handler */
-typedef struct
-{
-  unsigned long wwt;		/* Work waiting time (ms) */
-}
-Protocol_T14;
 
 /*
  * Exported functions declaration
  */
 
-/* Create a new protocol handler */
-extern Protocol_T0 *Protocol_T0_New (void);
-extern Protocol_T14 *Protocol_T14_New (void);
-
-/* Delete a protocol handler */
-extern void Protocol_T0_Delete (Protocol_T0 * t0);
-extern void Protocol_T14_Delete (Protocol_T14 * t14);
-
-/* Initialise a protocol handler */
-extern int Protocol_T0_Init (Protocol_T0 * t0, PPS_ProtocolParameters * params, int selected_protocol);
-extern int Protocol_T14_Init (Protocol_T14 * t14, PPS_ProtocolParameters * params, int selected_protocol);
-
 /* Send a command and return a response */
 extern int Protocol_T0_Command (APDU_Cmd * cmd, APDU_Rsp ** rsp);
 extern int Protocol_T14_Command (APDU_Cmd * cmd, APDU_Rsp ** rsp);
-
-/* Close a protocol handler */
-extern int Protocol_T0_Close (Protocol_T0 * t0);
-extern int Protocol_T14_Close (Protocol_T14 * t14);
 
 #endif /* _PROTOCOL_T0_ */
 
