9c9f68d984ad99aa81618bd115e61ac8b8cb8c4a
alno <alno@4b0bc96b-bc66-0410-9d44-ebda105a78c1>
2010-02-23 18:24:19 +0000

    WebIf: - Merge 1675, 1660-1661 of trunc
    git-svn-id: https://svn.streamboard.tv/oscam/branches/monitor-improvement@1662 4b0bc96b-bc66-0410-9d44-ebda105a78c1

--- module-cccam.c
+++ module-cccam.c
@@ -350,7 +350,7 @@ static int cc_msg_recv(uint8 *buf)
   }
 
   cc_crypt(&cc->block[DECRYPT], netbuf, 4, DECRYPT);
-  //cs_ddump(netbuf, 4, "cccam: decrypted header:");
+  cs_ddump(netbuf, 4, "cccam: decrypted header:");
 
   g_flag = netbuf[0];
 
@@ -1153,11 +1153,11 @@ static int cc_srv_connect()
   //cs_auth_client((struct s_auth *)(-1), NULL);
 
   // send passwd ack
-  bzero(data, 20);
-  memcpy(data, "CCcam\0", 6);
-  cs_ddump(data, 20, "cccam: send ack:");
-  cc_crypt(&cc->block[ENCRYPT], data, 20, ENCRYPT);
-  send(pfd, data, 20, 0);
+  bzero(buf, 20);
+  memcpy(buf, "CCcam\0", 6);
+  cs_ddump(buf, 20, "cccam: send ack:");
+  cc_crypt(&cc->block[ENCRYPT], buf, 20, ENCRYPT);
+  send(pfd, buf, 20, 0);
 
   // recv cli data
   bzero(buf,sizeof(buf));
@@ -1260,7 +1260,7 @@ int cc_cli_init()
 
     struct hostent *server;
     server = gethostbyname(reader[ridx].device);
-    bcopy((char *)server->h_addr, (char *)&client[cs_idx].udp_sa.sin_addr.s_addr, server->h_length);
+    memmove((char *)&client[cs_idx].udp_sa.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
 
     if (reader[ridx].tcp_rto <= 0) reader[ridx].tcp_rto = 60 * 60 * 10;  // timeout to 10 hours
     cs_debug("cccam: reconnect timeout set to: %d", reader[ridx].tcp_rto);
