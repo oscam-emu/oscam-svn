oscam-emu/oscam-svn

commit 7b00d38950da99d33073f2ade36ee0f8b60f0ddb

reader-viaccess: fix bug that could cause wrong control words
git-svn-id: https://svn.streamboard.tv/oscam/trunk@4821 4b0bc96b-bc66-0410-9d44-ebda105a78c1

reader-viaccess.c
@@ -292,7 +292,6 @@ static int viaccess_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
   // 0d -> post AES decrypt CW
   // 0b -> pre AES decrypt CW
   int nanoD2 = 0; //   0x0b = 1  0x0d = 2
-  int skipECM01 = 1;
 
   memset(DE04, 0, sizeof(DE04)); //fix dorcel de04 bug
 
@@ -364,12 +363,10 @@ static int viaccess_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
             // we can't assume that if the nano len is 5 or more we have an ecm number
             // as some card don't support this
             if( reader->last_geo.number_ecm > 0 ) {
-                if(reader->last_geo.number_ecm ==curnumber_ecm && ( ecm88Data[nanoLen-1] != 0x01 || skipECM01 == 0)) { //ecm88Data[8] != 0x01 try skip ECM 01 first pass
+                if (reader->last_geo.number_ecm == curnumber_ecm) {
                     keynr=ecm88Data[5];
                     cs_debug_mask(D_READER, "keyToUse = %02x, ECM ending with %02x",ecm88Data[5], ecm88Data[nanoLen-1]);
-                }
-                else
-                {
+                } else {
                     cs_debug_mask(D_READER, "Skip ECM ending with = %02x for ecm number (%x)",ecm88Data[nanoLen-1], curnumber_ecm);
                     ecm88Data=nextEcm;
                     ecm88Len-=curEcm88len;
@@ -440,8 +437,7 @@ static int viaccess_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
             } 
             // use AES from list to decrypt CW
             cs_debug_mask(D_READER, "Decoding CW : using AES key id %d for provider %06x",D2KeyID, (provid & 0xFFFFF0));
-            rc=aes_decrypt_from_list(reader->aes_list,0x500, (uint32) (provid & 0xFFFFF0), D2KeyID, &ecm88DataCW[0], 16);
-            if( rc == 0 )
+            if (aes_decrypt_from_list(reader->aes_list,0x500, (uint32) (provid & 0xFFFFF0), D2KeyID, &ecm88DataCW[0], 16) == 0)
                 snprintf( er->msglog, MSGLOGSIZE, "AES Decrypt : key id %d not found for CAID %04X , provider %06lx", D2KeyID, 0x500, (provid & 0xFFFFF0) );
         }
 
@@ -509,15 +505,6 @@ static int viaccess_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
         ecm88Len-=curEcm88len;
         cs_debug_mask(D_READER, "[viaccess-reader] ECM: Unknown ECM type");
         snprintf( er->msglog, MSGLOGSIZE, "Unknown ECM type" );
-    }
-    
-    //Try to parse again ECM without skiping ECM ending with 01
-    if( !(ecm88Len && !rc) && skipECM01 == 1)
-    {
-        skipECM01 = 0;
-        ecm88Data=er->ecm+4;
-        ecm88Len=SCT_LEN(er->ecm)-4;
-        nextEcm=ecm88Data;
     }
   }
 

commit 05040d5ec087e7485a9d9099b2ca0310aa51dc0d

temporary fix issue for TNTSAT (#1712 introduced in r4821)
git-svn-id: https://svn.streamboard.tv/oscam/trunk@4830 4b0bc96b-bc66-0410-9d44-ebda105a78c1

reader-viaccess.c
@@ -363,7 +363,7 @@ static int viaccess_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
             // we can't assume that if the nano len is 5 or more we have an ecm number
             // as some card don't support this
             if( reader->last_geo.number_ecm > 0 ) {
-                if (reader->last_geo.number_ecm == curnumber_ecm) {
+                if (reader->last_geo.number_ecm == curnumber_ecm && ( ecm88Data[nanoLen-1] != 0x01)) {
                     keynr=ecm88Data[5];
                     cs_debug_mask(D_READER, "keyToUse = %02x, ECM ending with %02x",ecm88Data[5], ecm88Data[nanoLen-1]);
                 } else {
@@ -437,7 +437,8 @@ static int viaccess_do_ecm(struct s_reader * reader, ECM_REQUEST *er)
             } 
             // use AES from list to decrypt CW
             cs_debug_mask(D_READER, "Decoding CW : using AES key id %d for provider %06x",D2KeyID, (provid & 0xFFFFF0));
-            if (aes_decrypt_from_list(reader->aes_list,0x500, (uint32) (provid & 0xFFFFF0), D2KeyID, &ecm88DataCW[0], 16) == 0)
+            rc=aes_decrypt_from_list(reader->aes_list,0x500, (uint32) (provid & 0xFFFFF0), D2KeyID, &ecm88DataCW[0], 16);
+            if( rc == 0 )
                 snprintf( er->msglog, MSGLOGSIZE, "AES Decrypt : key id %d not found for CAID %04X , provider %06lx", D2KeyID, 0x500, (provid & 0xFFFFF0) );
         }
 