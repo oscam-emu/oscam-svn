oscam-emu/oscam-svn

commit dc470b1f2ef6835aded7cb0eebc0dbc4a9f8e332

webif: Move error message to D_TRACE (1) level.
This moves "WebIf: Error opening file descriptor using fdopen()"
error message to TRACE level.

This message can be shown when a client tries to talk to SSL server
without using SSL which is hardly something we have to bother the
default logs by printing errors.

This "fixes" ticket #2851.

git-svn-id: https://svn.streamboard.tv/oscam/trunk@7696 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-webif.c
@@ -4910,7 +4910,9 @@ static void *serve_process(void *conn){
 						send_error(f, 200, "Bad Request", NULL, "This web server is running in SSL mode.", 1);
 					fflush(f);
 					fclose(f);
-				} else cs_log("WebIf: Error opening file descriptor using fdopen() (errno=%d %s)", errno, strerror(errno));
+-				} else {
+					cs_debug_mask(D_TRACE, "WebIf: fdopen(%d) failed. (errno=%d %s)", s, errno, strerror(errno));
+				}
 			}
 		} else cs_log("WebIf: Error calling SSL_set_fd().");
 		SSL_shutdown(ssl);

commit 0e322f1ce0c0645f59fee77a65e4329506c0f7d2

webif: Fix build error when WITH_SSL is enabled.
The bug was introduced in r7696.

git-svn-id: https://svn.streamboard.tv/oscam/trunk@7698 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-webif.c
@@ -4910,7 +4910,7 @@ static void *serve_process(void *conn){
 						send_error(f, 200, "Bad Request", NULL, "This web server is running in SSL mode.", 1);
 					fflush(f);
 					fclose(f);
--				} else {
+				} else {
 					cs_debug_mask(D_TRACE, "WebIf: fdopen(%d) failed. (errno=%d %s)", s, errno, strerror(errno));
 				}
 			}