oscam-emu/oscam-svn

commit be7fb1fc23dbfb406ebfb990e0cb71d1cb39335c

Remove CS_ANTICASC ifdef in oscam-failban.c
git-svn-id: https://svn.streamboard.tv/oscam/trunk@10232 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam-failban.c
@@ -1,4 +1,5 @@
 #include "globals.h"
+#include "module-anticasc.h"
 #include "oscam-net.h"
 #include "oscam-string.h"
 #include "oscam-time.h"
@@ -6,15 +7,12 @@
 static int32_t cs_check_v(IN_ADDR_T ip, int32_t port, int32_t add, char *info, int32_t acosc_penalty_duration)
 {
 	int32_t result = 0;
-	bool acosc_enabled = false;
 
-#ifdef CS_ANTICASC
-	if(cfg.acosc_enabled)
-		acosc_enabled = true;
-#endif
+	if (!acosc_enabled())
+		return 0;
 
-	if(!(cfg.failbantime || acosc_enabled))
-		{ return 0; }
+	if (!cfg.failbantime)
+		return 0;
 
 	if(!cfg.v_list)
 		{ cfg.v_list = ll_create("v_list"); }

module-anticasc.h
@@ -10,6 +10,7 @@ extern void ac_copy_vars(struct s_auth *src, struct s_auth *dst);
 extern void ac_init_client(struct s_client *cl, struct s_auth *account);
 extern void ac_chk(struct s_client *cl, ECM_REQUEST *er, int32_t level);
 extern void insert_zaplist(ECM_REQUEST *er, struct s_client *client);
+static inline bool acosc_enabled(void) { return cfg.acosc_enabled; }
 #else
 static inline void ac_init(void) { }
 static inline void ac_init_stat(void) { }
@@ -19,6 +20,7 @@ static inline void ac_copy_vars(struct s_auth *UNUSED(src), struct s_auth *UNUSE
 static inline void ac_init_client(struct s_client *UNUSED(cl), struct s_auth *UNUSED(account)) { }
 static inline void ac_chk(struct s_client *UNUSED(cl), ECM_REQUEST *UNUSED(er), int32_t UNUSED(level)) { }
 static inline void insert_zaplist(ECM_REQUEST *UNUSED(er), struct s_client *UNUSED(client)) { }
+static inline bool acosc_enabled(void) { return 0; }
 #endif
 
 #endif

commit 07a00a893808fecf3fb6e537e7d027fbe8965ab6

failban: Restore the behaviour broken by r10232.
git-svn-id: https://svn.streamboard.tv/oscam/trunk@10236 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam-failban.c
@@ -8,10 +8,7 @@ static int32_t cs_check_v(IN_ADDR_T ip, int32_t port, int32_t add, char *info, i
 {
 	int32_t result = 0;
 
-	if (!acosc_enabled())
-		return 0;
-
-	if (!cfg.failbantime)
+	if(!(cfg.failbantime || acosc_enabled()))
 		return 0;
 
 	if(!cfg.v_list)