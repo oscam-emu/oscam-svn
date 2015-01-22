oscam-emu/oscam-svn

commit f6a7fd2bebec25d1f42a5e8a300165b684210e5b

csctapi: Merge three write_settings() callbacks.
This patch removes write_settings2 and write_settings3 callbacks
merging them into single write_settings() that receives struct
s_cardreader_settings parameter.

git-svn-id: https://svn.streamboard.tv/oscam/trunk@10267 4b0bc96b-bc66-0410-9d44-ebda105a78c1

csctapi/icc_async.c
@@ -865,23 +865,26 @@ static int32_t InitCard(struct s_reader *reader, ATR *atr, unsigned char FI, uin
 		reader->worketu *= 2; // overclocked T14 needs this otherwise high ecm reponses
 	}
 
+	struct s_cardreader_settings s = {
+		.ETU = ETU,
+		.EGT = EGT,
+		.P   = 5,
+		.I   = I,
+		.F   = Fi,
+		.Fi  = (uint16_t) Fi,
+		.Di  = (unsigned char)D,
+		.Ni  = N,
+		.D   = D,
+		.WWT = WWT,
+		.BGT = BGT,
+	};
+
+	if (reader->cardmhz < 2000)
+		s.Fi = F/100; // non pll internal reader needs base frequency like 1,2,3,4,5,6 MHz not clock rate conversion factor (Fi)
+
 	if(reader->crdr.write_settings)
 	{
-		call(reader->crdr.write_settings(reader, ETU, EGT, 5, I, (uint16_t) Fi, (unsigned char)D, N));
-	}
-	else if(reader->crdr.write_settings2)
-	{
-		call(reader->crdr.write_settings2(reader, (uint16_t) Fi, (uint8_t) D, WWT, EGT, BGT));
-	}
-	else if(reader->crdr.write_settings3)
-	{
-//#if defined(__SH4__) || defined(STB04SCI) || defined(__powerpc__)
-		if (reader->cardmhz < 2000)
-		{
-		Fi = F/100; // non pll internal reader needs base frequency like 1,2,3,4,5,6 MHz not clock rate conversion factor (Fi)
-		}
-//#endif
-		call(reader->crdr.write_settings3(reader, ETU, Fi, WWT, reader->CWT, reader->BWT, EGT, (unsigned char)I));
+		call(reader->crdr.write_settings(reader, &s));
 	}
 
 	if(reader->typ == R_INTERNAL)

csctapi/ifd_sci.c
@@ -520,7 +520,7 @@ static int32_t Sci_Close(struct s_reader *reader)
 	return OK;
 }
 
-static int32_t sci_write_settings3(struct s_reader *reader, uint32_t ETU, uint32_t F, uint32_t WWT, uint32_t CWT, uint32_t BWT, uint32_t EGT, uint32_t I)
+static int32_t sci_write_settings(struct s_reader *reader, struct s_cardreader_settings *s)
 {
 	if(reader->cardmhz > 2000)   // only for dreambox internal readers
 	{
@@ -528,13 +528,13 @@ static int32_t sci_write_settings3(struct s_reader *reader, uint32_t ETU, uint32
 		if(reader->protocol_type != ATR_PROTOCOL_TYPE_T14)   // fix VU+ internal reader slow responses on T0/T1
 		{
 			cs_sleepms(150);
-			call(Sci_WriteSettings(reader, 0, reader->divider, ETU, WWT, CWT, BWT, EGT, 5, (unsigned char)I));
+			call(Sci_WriteSettings(reader, 0, reader->divider, s->ETU, s->WWT, reader->CWT, reader->BWT, s->EGT, 5, (unsigned char)s->I));
 			cs_sleepms(150);
 		}
 		else     // no fixup for T14 protocol otherwise error
 		{
 			cs_sleepms(150);
-			call(Sci_WriteSettings(reader, reader->protocol_type, reader->divider, ETU, WWT, CWT, BWT, EGT, 5, (unsigned char)I));
+			call(Sci_WriteSettings(reader, reader->protocol_type, reader->divider, s->ETU, s->WWT, reader->CWT, reader->BWT, s->EGT, 5, (unsigned char)s->I));
 			cs_sleepms(150);
 		}
 	}
@@ -542,7 +542,7 @@ static int32_t sci_write_settings3(struct s_reader *reader, uint32_t ETU, uint32
 	{
 		// P fixed at 5V since this is default class A card, and TB is deprecated
 		cs_sleepms(150);
-		call(Sci_WriteSettings(reader, reader->protocol_type, F, ETU, WWT, CWT, BWT, EGT, 5, (unsigned char)I));
+		call(Sci_WriteSettings(reader, reader->protocol_type, s->F, s->ETU, s->WWT, reader->CWT, reader->BWT, s->EGT, 5, (unsigned char)s->I));
 		cs_sleepms(150);
 	}
 	return OK;
@@ -560,7 +560,7 @@ void cardreader_internal_sci(struct s_cardreader *crdr)
 	crdr->transmit     = IO_Serial_Transmit;
 	crdr->receive      = IO_Serial_Receive;
 	crdr->close        = Sci_Close;
-	crdr->write_settings3 = sci_write_settings3;
+	crdr->write_settings = sci_write_settings;
 }
 
 #endif

csctapi/ifd_cool.c
@@ -212,7 +212,7 @@ static void Cool_Print_Comm_Parameters(struct s_reader *reader)
 
 }
 
-static int32_t Cool_WriteSettings(struct s_reader *reader, uint16_t F, uint8_t D, uint32_t WWT, uint32_t EGT, uint32_t BGT)
+static int32_t Cool_WriteSettings(struct s_reader *reader, struct s_cardreader_settings *s)
 {
 	struct cool_data *crdr_data = reader->crdr_data;
 	//first set freq back to reader->mhz if necessary
@@ -234,13 +234,13 @@ static int32_t Cool_WriteSettings(struct s_reader *reader, uint16_t F, uint8_t D
 			{ BLKTime = (reader->BWT - 11); }
 		if(reader->CWT > 11)
 			{ CHTime = (reader->CWT - 11); }
-		if(BGT > 11)
-			{ BKGuardTime = (BGT - 11); }
+		if(s->BGT > 11)
+			{ BKGuardTime = (s->BGT - 11); }
 		else
 			{ BKGuardTime = 11; } //For T1, the BGT minimum time shall be 22 work etus. BGT is effectively offset by 11 etus internally.
 		if(!crdr_data->pps)
 		{
-			ret = cnxt_smc_set_F_D_factors(crdr_data->handle, F, D);
+			ret = cnxt_smc_set_F_D_factors(crdr_data->handle, s->F, s->D);
 			coolapi_check_error("cnxt_smc_set_F_D_factors", ret);
 		}
 		break;
@@ -248,10 +248,10 @@ static int32_t Cool_WriteSettings(struct s_reader *reader, uint16_t F, uint8_t D
 	case ATR_PROTOCOL_TYPE_T14:
 	default:
 		BLKTime = 0;
-		if(WWT > 12)
-			{ CHTime = (WWT - 12); }
-		if(BGT > 12)
-			{ BKGuardTime = (BGT - 12); }
+		if(s->WWT > 12)
+			{ CHTime = (s->WWT - 12); }
+		if(s->BGT > 12)
+			{ BKGuardTime = (s->BGT - 12); }
 		if(BKGuardTime < 4)
 			{ BKGuardTime = 4; } //For T0, the BGT minimum time shall be 16 work etus. BGT is effectively offset by 12 etus internally.
 		if(!crdr_data->pps)
@@ -262,7 +262,7 @@ static int32_t Cool_WriteSettings(struct s_reader *reader, uint16_t F, uint8_t D
 			}
 			else
 			{
-				ret = cnxt_smc_set_F_D_factors(crdr_data->handle, F, D);
+				ret = cnxt_smc_set_F_D_factors(crdr_data->handle, s->F, s->D);
 			}
 			coolapi_check_error("cnxt_smc_set_F_D_factors", ret);
 		}
@@ -276,7 +276,7 @@ static int32_t Cool_WriteSettings(struct s_reader *reader, uint16_t F, uint8_t D
 	coolapi_check_error("cnxt_smc_get_config_timeout", ret);
 	timeout.BLKTime = BLKTime;
 	timeout.CHTime = CHTime;
-	timeout.CHGuardTime = EGT;
+	timeout.CHGuardTime = s->EGT;
 	timeout.BKGuardTime = BKGuardTime;
 	ret = cnxt_smc_set_config_timeout(crdr_data->handle, timeout);
 	coolapi_check_error("cnxt_smc_set_config_timeout", ret);
@@ -326,7 +326,7 @@ void cardreader_internal_cool(struct s_cardreader *crdr)
 	crdr->transmit     = Cool_Transmit;
 	crdr->receive      = Cool_Receive;
 	crdr->close        = Cool_Close;
-	crdr->write_settings2 = Cool_WriteSettings;
+	crdr->write_settings = Cool_WriteSettings;
 	crdr->set_protocol  = Cool_SetProtocol;
 }
 

csctapi/ifd_smargo.c
@@ -84,9 +84,9 @@ static int32_t smargo_set_settings(struct s_reader *reader, int32_t freq, unsign
 	return OK;
 }
 
-static int32_t smargo_writesettings(struct s_reader *reader, uint32_t UNUSED(ETU), uint32_t UNUSED(EGT), unsigned char UNUSED(P), unsigned char UNUSED(I), uint16_t Fi, unsigned char Di, unsigned char Ni)
+static int32_t smargo_write_settings(struct s_reader *reader, struct s_cardreader_settings *s)
 {
-	return smargo_set_settings(reader, reader->mhz, reader->protocol_type == 1 ? 0 : reader->protocol_type , reader->convention, Fi, Di, Ni);
+	return smargo_set_settings(reader, reader->mhz, reader->protocol_type == 1 ? 0 : reader->protocol_type , reader->convention, s->Fi, s->Di, s->Ni);
 }
 
 
@@ -266,7 +266,7 @@ void cardreader_smargo(struct s_cardreader *crdr)
 	crdr->transmit  = IO_Serial_Transmit;
 	crdr->receive       = IO_Serial_Receive;
 	crdr->close     = IO_Serial_Close;
-	crdr->write_settings = smargo_writesettings;
+	crdr->write_settings = smargo_write_settings;
 	crdr->set_parity    = IO_Serial_SetParity;
 	crdr->typ       = R_MOUSE;
 

csctapi/ifd_smartreader.c
@@ -1779,16 +1779,9 @@ int32_t SR_Activate(struct s_reader *reader, struct s_ATR *atr)
 	return OK;
 }
 
-int32_t sr_write_settings(struct s_reader *reader,
-						  uint32_t UNUSED(ETU),
-						  uint32_t EGT,
-						  unsigned char UNUSED(P),
-						  unsigned char UNUSED(I),
-						  uint16_t Fi,
-						  unsigned char Di,
-						  unsigned char UNUSED(Ni))
+int32_t sr_write_settings(struct s_reader *reader, struct s_cardreader_settings *s)
 {
-	SR_WriteSettings(reader, Fi, Di, EGT, (unsigned char)reader->protocol_type, reader->convention);
+	SR_WriteSettings(reader, s->Fi, s->Di, s->EGT, (unsigned char)reader->protocol_type, reader->convention);
 	return OK;
 }
 

csctapi/ifd_stapi.c
@@ -70,15 +70,9 @@ static int32_t stapi_setprotocol(struct s_reader *reader, unsigned char *params,
 	return STReader_SetProtocol(crdr_data->stapi_handle, params, length, len_request);
 }
 
-static int32_t stapi_writesettings(struct s_reader *reader, uint32_t ETU, uint32_t EGT, unsigned char P, unsigned char I, uint16_t Fi, unsigned char Di, unsigned char Ni)
+static int32_t stapi_writesettings(struct s_reader *reader, struct s_cardreader_settings *s)
 {
-	(void)ETU;
-	(void)EGT;
-	(void)P;
-	(void)I;
-	(void)Fi;
-	(void)Di;
-	(void)Ni;
+	(void)s;
 	struct stapi_data *crdr_data = reader->crdr_data;
 	return STReader_SetClockrate(crdr_data->stapi_handle);
 }

globals.h
@@ -726,6 +726,21 @@ struct s_module
 
 struct s_ATR ;
 
+struct s_cardreader_settings
+{
+	uint32_t ETU;
+	uint32_t EGT;
+	unsigned char P;
+	uint32_t I;
+	uint32_t F;
+	uint32_t Fi;
+	unsigned char Di;
+	unsigned char Ni;
+	uint32_t WWT;
+	uint32_t BGT;
+	uint8_t D;
+};
+
 struct s_cardreader
 {
 	char            *desc;
@@ -739,19 +754,7 @@ struct s_cardreader
 	void (*unlock)(struct s_reader *);
 	int32_t (*close)(struct s_reader *);
 	int32_t (*set_parity)(struct s_reader *, uchar parity);
-	// FIXME: All parameters passed to write_settingsX should be put in a struct
-	int32_t (*write_settings)(struct s_reader *,
-							  uint32_t ETU,
-							  uint32_t EGT,
-							  unsigned char P,
-							  unsigned char I,
-							  uint16_t Fi,
-							  unsigned char Di,
-							  unsigned char Ni);
-	// FIXME: write_settings2 is used by coolstream reader
-	int32_t (*write_settings2)(struct s_reader *, uint16_t F, uint8_t D, uint32_t WWT, uint32_t EGT, uint32_t BGT);
-	// FIXME: write_settings3 is used by sci reader
-	int32_t (*write_settings3)(struct s_reader *, uint32_t ETU, uint32_t F, uint32_t WWT, uint32_t CWT, uint32_t BWT, uint32_t EGT, uint32_t I);
+	int32_t (*write_settings)(struct s_reader *, struct s_cardreader_settings *s);
 	int32_t (*set_protocol)(struct s_reader *,
 							unsigned char *params,
 							uint32_t *length,

commit 0f4c5056196b573f8ace1cedb38a4f41614d54e9

csctapi: Change Fi only on sci card readers.
This fixes regression introduced in r10267.

git-svn-id: https://svn.streamboard.tv/oscam/trunk@10268 4b0bc96b-bc66-0410-9d44-ebda105a78c1

csctapi/icc_async.c
@@ -879,9 +879,6 @@ static int32_t InitCard(struct s_reader *reader, ATR *atr, unsigned char FI, uin
 		.BGT = BGT,
 	};
 
-	if (reader->cardmhz < 2000)
-		s.Fi = F/100; // non pll internal reader needs base frequency like 1,2,3,4,5,6 MHz not clock rate conversion factor (Fi)
-
 	if(reader->crdr.write_settings)
 	{
 		call(reader->crdr.write_settings(reader, &s));

csctapi/ifd_sci.c
@@ -522,6 +522,9 @@ static int32_t Sci_Close(struct s_reader *reader)
 
 static int32_t sci_write_settings(struct s_reader *reader, struct s_cardreader_settings *s)
 {
+	if (reader->cardmhz < 2000)
+		s->Fi = s->F / 100; // non pll internal reader needs base frequency like 1,2,3,4,5,6 MHz not clock rate conversion factor (Fi)
+
 	if(reader->cardmhz > 2000)   // only for dreambox internal readers
 	{
 		// P fixed at 5V since this is default class A card, and TB is deprecated